cc41dcfd525daf0193fdf4f52c0adbae7b611a18
sourav3090 <souravsingh278@gmail.com>
2026-05-06 14:19:21 +0530

    update files

---
+++ 01-assignment-variables/10-profit-loss.py
@@ -0,0 +1,15 @@
+# 10. Calculate profit or loss percentage
+cost_price = 500
+selling_price = 600
+
+print(f"The Cost Price is: {cost_price}")
+print(f"The Selling Price is: {selling_price}")
+print()
+
+profit = selling_price - cost_price
+print(f"The profit is (Rs.): {profit}")
+print()
+
+percentage = (profit / cost_price) * 100
+
+print(f"The Profit is (%): {percentage}")
