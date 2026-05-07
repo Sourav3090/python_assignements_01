26292910af1b6da7a63f2f3edd23dadecf982530
sourav3090 <souravsingh278@gmail.com>
2026-05-07 14:14:58 +0530

    update files

---
+++ 01-assignment-variables/17-farehanite-conversion.py
@@ -0,0 +1,9 @@
+# 17. Convert 20°C to Fahrenheit 
+# Fahrenheit = (celsius * 9/5) + 32
+celsius=20
+print(f"Before Conversion(°C):{celsius}")
+print("="*45)
+print()
+
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"After Conversion into Fahrenheit: {int(fahrenheit)}")
